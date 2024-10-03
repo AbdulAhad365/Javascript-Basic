05911c7ddded12e6014d9ebfb6f0d4a9ee66401c
AbdulAHad365 <amir.raza537918@gmail.com>
2024-10-03 14:54:26 -0400

    Added sc.js file

---
+++ sc.js
@@ -0,0 +1,16 @@
+// TO CALCULATE THE BMI OF THESE.........
+
+//for ali
+const mass=prompt("ENTER THE MASS IN KG FOR ALI")
+const height=prompt("ENTER THE HEIGHT IN METER FOR ALI")
+
+//for ahmad
+const mass1=prompt("ENTER THE MASS IN KG")
+const height1=prompt("ENTER THE HEIGHT IN METER")
+
+//BMI
+//ali
+BMI=mass/(height**2)
+//ahmad
+BMI1=mass1/(height1**2)
+//compare them and print the heighest one of them
