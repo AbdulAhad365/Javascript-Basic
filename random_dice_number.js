6b657f52ef43a774e0065dccdbf941c3647fe3af
AbdulAHad365 <amir.raza537918@gmail.com>
2024-10-05 00:50:35 -0400

    random dice number gernator

---
+++ random_dice_number.js
@@ -0,0 +1,9 @@
+// console.log(rand)
+let rand=0
+while(rand!=6){
+    rand=Math.trunc(Math.random()*7)
+    console.log("The dice is "+rand)
+    if (rand===6){
+        console.log("-------I GOT 6------")
+    }
+}
